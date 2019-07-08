d70f95e7cce6843a04e6d8066ac074717d9b4a01
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-07-08 16:29:10 -0700

    conditional render draft

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
-import Profile from './Components/Profile/Profile'
+import Profile from './Components/Profile/Profile';
 import MarketPlace from './Components/MarketPlace/MarketPlace';
 import "./reset.css";
 import "./App.scss";
