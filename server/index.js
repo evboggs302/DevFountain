3631e0c99af51f7897058e1b97cc957a76b0570b
HSR <hectorsilvarobles@gmail.com>
2019-07-08 11:18:28 -0700

    working on nodemailer

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 app.use(express.json());
 const session = require("express-session");
 const nodemailer = require("nodemailer");
-const { login, register, userInfo, logout } = require("./controller/");
-const { SERVER_PORT } = process.env;
+const { login, register, userInfo, logout } = require("./controllers/");
+const { SERVER_PORT, EMAIL, EMAIL_PASSWORD } = process.env;
 
 app.use(express.json());
 app.use(
@@ -30,6 +30,34 @@ app.post("/api/login", login);
 app.post("/api/register", register);
 app.get("/api/user", userInfo);
 app.get("/api/logout", logout);
+
+// Nodemailer
+app.post('/api/send', (req, res, next) => {
+  const {name, email, title, message} = req.body
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: EMAIL,
+      pass: EMAIL_PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from:`${EMAIL}`,
+    to: `${email}`,
+    subject: `${title} by ${name}`,
+    text: `${message}`,
+    replyTo: ``
+  };
+
+  transporter.sendMail(mailOptions, (err, res) => {
+    if(err){
+      console.error('there was an error ', err)
+    } else{
+      console.log('here is the email', res)
+    }
+  })
+})
 
 const port = SERVER_PORT || 4000;
 console.log(port);
