bc578ca2cbddfb8e06985d1c42e548221be75ec7
HSR <hectorsilvarobles@gmail.com>
2019-07-08 13:25:30 -0700

    added a network reducer

--- src/dux/store.js
+++ src/dux/store.js
@@ -3,13 +3,15 @@ import {createStore, combineReducers } from 'redux'
 import userReducer from './reducers/userReducer'
 import messageReducer from './reducers/messageReducer'
 import postsReducer from './reducers/postsReducer'
+import networkReducer from './reducers/networkReducer'
 
 
 const rootReducer = combineReducers(
     {
         userReducer: userReducer,
         messageReducer: messageReducer,
-        postsReducer: postsReducer
+        postsReducer: postsReducer,
+        networkReducer: networkReducer
     })
 
 export default createStore(rootReducer)
