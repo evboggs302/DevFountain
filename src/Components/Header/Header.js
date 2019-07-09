708790ef996ca40ee9d4618e7fd72cfa4a761a42
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-07-09 16:43:33 -0700

    login functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { setUser } from '../../dux/reducers/userReducer';
+import { connect } from 'react-redux'
+import axios from 'axios'
 
 class Header extends Component {
+
+componentDidMount() {
+    axios.get('/api/user').then(res => {
+        // console.log(res.data);
+        this.props.setUser(res.data)
+    })
+}
+
+login = () => {
+  const {email, password} = this.props.userReducer.user
+  axios.post('/api/login', {email, password}).then(res => {
+    this.props.setUser(res.data);
+  })
+}
+
+
   render() {
     console.log(this.props)
-    const { email, first, last } = this.props.user;
+    const { email, first, last } = this.props.userReducer.user;
     return (
       <div>
         {!this.props.user ? (
@@ -22,4 +41,19 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = (reduxState) => {
+  return reduxState
+}
+
+const mapDispatchToProps = {
+  setUser
+}
+
+const invokedConnect = connect (
+  mapStateToProps,
+  mapDispatchToProps
+)
+
+
+
+export default invokedConnect(Header);
