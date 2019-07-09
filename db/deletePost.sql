36c143a7f27447690705ebc3ce929ff8770b735e
Joseph Esgar <josephesgar@Josephs-MacBook-Air.local>
2019-07-09 10:15:12 -0700

    post controller finished

--- db/deletePost.sql
+++ db/deletePost.sql
@@ -1,5 +1,8 @@
+delete from postlikes
+where post_id = $2;
+
 delete from posts 
-where user_id = $1;
+where user_id = $1 and post_id = $2;
 
 select *
 from posts
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,53 +1,72 @@
 module.exports = {
-    getPosts: (req, res, next) => {
-        // pass a user id to get all of that user's posts
-        const {email} = req.params;
-        const db = req.app.get("db");
-        db.getallpostsone(email).then(posts => {
-            db.getallpoststwo(posts[0].user_id).then(morePosts => {
-                res.status(200).send(morePosts)
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send("We had a problem getting posts. Try later.")
-        })
-    },
+  getPosts: (req, res, next) => {
+    // pass an email to get all of that user's posts
+    const { email } = req.params;
+    const db = req.app.get("db");
+    // post one takes an email address to get a user and then passes that user_id into post two
+    db.getallpostsone(email)
+      .then(user => {
+        // post two takes in a user_id and gets their posts.
+        db.getallpoststwo(user[0].user_id).then(morePosts => {
+          res.status(200).send(morePosts);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("We had a problem getting posts. Try later.");
+      });
+  },
 
-    createPost: (req, res, next) => {
-        const{content} = req.body;
-        const {user_id} = req.session.user
-        const db = req.app.get("db")
-        db.makePost([content, new Date(), user_id]).then(newPosts => {
-            res.status(200).send(newPosts)
-        }).catch(err => {
-            console.log(err)
-            res.status(500).send("Server error oops!")
-        })
-    },
+  createPost: (req, res, next) => {
+    // the content of the message comes from the req.body.
+    const { content } = req.body;
+    // the user_id comes off the req.session.user
+    const { user_id } = req.session.user;
+    const db = req.app.get("db");
+    // a new date is created with the date constructor
+    db.makePost([content, new Date(), user_id])
+      .then(newPosts => {
+        res.status(200).send(newPosts);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("Server error oops!");
+      });
+  },
 
-    changePost: (res, res, next) => {
-        const {post_id} = req.params;
-        const {user_id} = req.session.user;
-        const {content} = req.body;
-        const db = req.app.get("db");
-        db.editPost([content, post_id, user_id]).then(updatedPosts => {
-            res.status(200).send(updatedPosts);
-        }).catch(err => {
-            console.log(err)
-            res.status(500).send("Sorry, try later.")
-        })
-    },
-    removePost: (req, res, next ) => {
-        const {user_id} = req.sesstion.user
-        const {} = req.body;
-        console.log(user_id);
-        const db = req.app.get("db");
-        db.deletePost(user_id).then(remainingPosts => {
-            res.status(200).send(remainingPosts)
-        }).catch(err => {
-            console.log(err)
-            res.status(500).send("Sorry an error happened.")
-        })
-    }
-}
+  changePost: (req, res, next) => {
+    // the post_id comes off the params
+    const { id } = req.params;
+    // user_id comes off the user object on session
+    const { user_id } = req.session.user;
+    // content comes off the req.body
+    const { content } = req.body;
+    const db = req.app.get("db");
+    db.editPost([content, id, user_id])
+      .then(updatedPosts => {
+        res.status(200).send(updatedPosts);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("Sorry, try later.");
+      });
+  },
+
+  removePost: (req, res, next) => {
+    // you know the drill
+    const { user_id } = req.session.user;
+    // id is still the post_id
+    const { id } = req.params;
+    console.log(user_id, id);
+    const {} = req.body;
+    const db = req.app.get("db");
+    db.deletePost([user_id, id])
+      .then(remainingPosts => {
+        res.status(200).send(remainingPosts);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("Sorry an error happened.");
+      });
+  }
+};
