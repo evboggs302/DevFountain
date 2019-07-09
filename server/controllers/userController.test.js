6f95c794f26ea9171d992b533eb36d17dc9440d3
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-07-09 13:56:02 -0700

    register integration test

--- server/controllers/userController.test.js
+++ server/controllers/userController.test.js
@@ -4,11 +4,42 @@ const sinon = require("sinon");
 const sandbox = sinon.createSandbox();
 
 describe("integration tests", () => {
+  let db;
+  function clearDatabase() {
+    return db.query("DELETE FROM movies");
+  }
+
   beforeAll(() => {}), beforeEach(() => {}), afterAll(() => {});
 });
 
-describe("create", () => {
-  it("responds with new user", done => {});
+// EVAN
+describe("register new user success", () => {
+  it("responds with new user", done => {
+    const req = {
+      app: {
+        get: () => db
+      },
+      body: {
+        first: "Evan",
+        last: "Boggs",
+        developer: true,
+        email: "evan@evan.com",
+        password: "eboggs123456789"
+      }
+    };
+    const res = {
+      json: function(data) {
+        expect(data).toMatchObject({
+          first: "Evan",
+          last: "Boggs",
+          developer: true,
+          email: "evan@evan.com"
+        });
+        done();
+      }
+    };
+    userController.register(req, res);
+  });
 
   it("responds with an error on user already existing", done => {});
 });
