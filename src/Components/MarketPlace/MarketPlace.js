0cdb96ce481474547a309437a6d671a040726343
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-07-09 09:41:21 -0700

    put email in the routing match object

--- src/Components/MarketPlace/MarketPlace.js
+++ src/Components/MarketPlace/MarketPlace.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import DevMarketPlace from './DevMarketPlace';
-import RecMarketPlace from './RecMarketPlace';
-
+import React from "react";
 
 function MarketPlace(props) {
-    return (
-        <div>
-            {props.user.user.developer ?
-            <DevMarketPlace/> :
-            <RecMarketPlace/>
-            }
-        </div>
-    )
+  //   getSingleUser = (email) => {
+  //     const encoded = encodeURIComponent(email)
+  //   };
+
+  //   mappedDevs = alldevsfromDB.map(dev => {
+  //     return (
+  //       <div key={dev.id}>
+  //         <Link to={`/api/profile/${dev.email}`}>
+  //           <img
+  //             src={dev.img}
+  //             alt="image"
+  //             // onClick={() => {
+  //             //   getSingleUser(dev.email);
+  //             // }}
+  //           >
+  //             Dev profile pic
+  //           </img>
+  //         </Link>
+  //         <h3>Dev Name</h3>
+  //         <div>Dev Skills</div>
+  //       </div>
+  //     );
+  //   });
+
+  return <div>Mapped Devs{/* {mappedDevs} */}</div>;
 }
 
-export default MarketPlace
+export default MarketPlace;
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import RecProfile from './RecProfile';
-import DevProfile from './DevProfile';
+import React from "react";
+import RecProfile from "./RecProfile";
+import DevProfile from "./DevProfile";
 
 function Profile(props) {
-    return (
-        <div>
-        {/* conditionally rending whether the user is a developer. If the user is not a developer, profile will render the
+  return (
+    <div>
+      {/* conditionally rending whether the user is a developer. If the user is not a developer, profile will render the
         recruiter profile */}
-        {props.user.user.developer ?
-        <DevProfile />  :
-        <RecProfile/>      
-        }
-        </div>
-    )
+      {props.user.user.developer ? <DevProfile /> : <RecProfile />}
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
