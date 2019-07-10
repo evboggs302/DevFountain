048b8b17ba15b4a820a3926220fff1b67836c459
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-07-10 11:15:45 -0700

    unit test example for hooks

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
-import RegisterForm from './RegisterForm/RegisterForm'
-import Header from '../Header/Header'
+import React, { useState } from "react";
+import RegisterForm from "./RegisterForm/RegisterForm";
+import Header from "../Header/Header";
 
-
-class Home extends Component {
-    
-    render () {
-        // console.log(props)
-        return (
-            <div>
-                <div>
-                    {/* <Header/> */}
-                </div>
-                Home
-                <RegisterForm />
-            </div>
-        )
-    }
-   
+function Home(props) {
+  // console.log(props)
+  return (
+    <div>
+      <div>{/* <Header/> */}</div>
+      Home
+      <RegisterForm />
+    </div>
+  );
 }
 
-
-export default Home
+export default Home;
