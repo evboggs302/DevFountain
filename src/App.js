aceb0e6df0c37474eaa02b1d46b67b350df42560
HSR <hectorsilvarobles@gmail.com>
2019-07-10 09:58:26 -0700

    made some updates to the marketplace hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from "react";
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
@@ -10,17 +9,6 @@ import {connect} from 'react-redux'
 import {setSkills} from './dux/reducers/skillsReducer'
 import UseFetch from './Components/usefetch'
 import Axios from "axios";
-=======
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import Profile from "./Components/Profile/Profile";
-import MarketPlace from "./Components/MarketPlace/MarketPlace";
-import "./reset.css";
-import "./App.scss";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
->>>>>>> 45d393da3e13b25de4735385045b9604bbec2048
 
 function App(props) {
   const {data: skillsData, fetchData: setSkills}= UseFetch('/api/allskills')
@@ -39,15 +27,7 @@ function App(props) {
         <Route path="/marketplace" component={MarketPlace} />
       </Switch>
     </div>
-<<<<<<< HEAD
-  )
-}
-
-const mapPropsToState = (reduxState) => {
-  return reduxState
-=======
   );
->>>>>>> 45d393da3e13b25de4735385045b9604bbec2048
 }
 
 const mappedDispatchToProps = {
--- src/Components/MarketPlace/MarketPlace.js
+++ src/Components/MarketPlace/MarketPlace.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState, useEffect} from "react";
 import axios from 'axios'
 import {setDevelopers} from '../../dux/reducers/marketplaceReducer'
@@ -35,72 +34,6 @@ function MarketPlace(props) {
       })
     }
    
-=======
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { setDevelopers } from "../../dux/reducers/marketplaceReducer";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-
-function MarketPlace(props) {
-  const [allDevs, setDevs] = useState([]);
-  useEffect(() => {
-    axios
-      .get("/api/marketplace")
-      .then(res => {
-        console.log(res.data);
-        setDevs(res.data);
-      })
-      .catch(err => console.log("this is the error", err));
-  }, []);
-
-  useEffect(() => {
-    props.setDevelopers(allDevs);
-  }, [allDevs]);
-
-  // Rendering each developers info on marketplace
-  const developers = props.marketplaceReducer.allDevelopers;
-  let mappedDevs;
-  if (developers.length > 0) {
-    mappedDevs = developers.map(dev => {
-      const encoded = encodeURIComponent(dev.email);
-      return (
-        <Link to={`/api/profile/${encoded}`}>
-          <div key={dev.user_id}>
-            <img src={dev.profile_pic} />
-            <h1>{`${dev.first} ${dev.last}`}</h1>
-            <h2>{dev.title}</h2>
-            <h2>{dev.email}</h2>
-          </div>
-        </Link>
-      );
-    });
-  }
-
-  //   let mappedDevs;
-  //     {props.allDevelopers ?
-  //       mappedDevs = props.marketplaceReducer.allDevelopers.map(dev => {
-
-  //       const encoded = encodeURIComponent(dev.email)
-  //     return (
-  //       <div key={dev.id}>
-  //         <Link to={`/api/profile/${encoded}`}>
-  //           <img
-  //             src={dev.profile_pic}
-  //             alt="image"
-  //           >
-  //             Dev profile pic
-  //           </img>
-  //         </Link>
-  //         <h3>{`${dev.first} ${dev.last}`}</h3>
-  //         <div>Dev Skills</div>
-  //       </div>
-  //     );
-  //   })
-  //   : null
-  // }
-
->>>>>>> 45d393da3e13b25de4735385045b9604bbec2048
   return (
     <div>
       Mapped Devs
