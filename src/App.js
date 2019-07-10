d682a1197f1a5ce971fbce253a7973baa245fb7b
HSR <hectorsilvarobles@gmail.com>
2019-07-09 17:08:59 -0700

    Trying to get skills subscribed to reducer

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Profile from './Components/Profile/Profile';
 import MarketPlace from './Components/MarketPlace/MarketPlace';
 import "./reset.css";
 import "./App.scss";
+import {connect} from 'react-redux'
+import {setSkills} from './dux/reducers/skillsReducer'
+import UseFetch from './Components/usefetch'
 
 function App() {
+  const {data: skills, fetchData: getSkills} = UseFetch('/api/allskills')
+  console.log(skills, getSkills)
   return (
     <div className="App">
       <Switch>
@@ -17,7 +22,16 @@ function App() {
 
     </div>
   )
-
 }
 
-export default App;
+const mapPropsToState = (reduxState) => {
+  return reduxState
+}
+
+const mappedDispatchToProps = {
+  setSkills
+}
+
+const myConnect = connect(mapPropsToState, mappedDispatchToProps)
+
+export default myConnect(App);
