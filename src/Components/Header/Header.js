97896e8b8cf4b28a7d9e8813250ac63b8df5b62c
Evan Boggs Jr <evanboggs@Evans-MacBook-Pro.local>
2019-07-10 17:25:28 -0700

    working on testing the hooks

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,47 +1,48 @@
-import React, {useState, useEffect } from "react";
-import {Redirect} from 'react-router-dom'
-import usefetch from "../usefetch";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import usefetch from "../useFetch";
 import { setUser } from "../../dux/reducers/userReducer";
 import { connect } from "react-redux";
 
-
 function Header(props) {
   //calling usefetch and destructering "fetchdata" and "postdata" using aliases userData for fetchdata and login for postData.
-    const {data: userData, postData: login }= usefetch('/api/user')
+  const { data: userData, postData: login } = usefetch("/api/user");
 
   //
-    useEffect(() => {
+  useEffect(() => {
     //
-    props.setUser(userData)
-    }, [userData])
-  
+    props.setUser(userData);
+  }, [userData]);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [redirect, setRedirect] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-    // console.log(this.props);
-    const { first, last } = props.userReducer.user;
-    if(redirect) {
-      return <Redirect to='/profile'/>
-    }
-    return (
-      <div>
-        {!props.userReducer.user ? (
-          <div>
-            Email:
-            <input onChange={e => setEmail(e.target.value)} />
-            Password:
-            <input onChange={e => setPassword(e.target.value)} />
-            <button onClick={() => login(email, password).then(setRedirect(true))}>Login</button>
-          </div>
-        ) : (
-          <div>{`Welcome, ${first} ${last}`}</div>
-        )}
-      </div>
-    );
+  // console.log(this.props);
+  const { first, last } = props.userReducer.user;
+  if (redirect) {
+    return <Redirect to="/profile" />;
   }
-
+  return (
+    <div>
+      {!props.userReducer.user ? (
+        <div>
+          Email:
+          <input onChange={e => setEmail(e.target.value)} />
+          Password:
+          <input onChange={e => setPassword(e.target.value)} />
+          <button
+            onClick={() => login(email, password).then(setRedirect(true))}
+          >
+            Login
+          </button>
+        </div>
+      ) : (
+        <div>{`Welcome, ${first} ${last}`}</div>
+      )}
+    </div>
+  );
+}
 
 const mapStateToProps = reduxState => {
   return reduxState;
--- src/Components/MarketPlace/MarketPlace.js
+++ src/Components/MarketPlace/MarketPlace.js
@@ -1,42 +1,38 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios'
-import {setDevelopers} from '../../dux/reducers/marketplaceReducer'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import UseFetch from '../usefetch'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { setDevelopers } from "../../dux/reducers/marketplaceReducer";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import UseFetch from "../useFetch";
 import { ToastContainer } from "react-toastify";
 
-
-
 function MarketPlace(props) {
-
-  // Setting all users who are developers into the marketplaceReducer 
-  const {data: devUsers, fetchData: allDevs}= UseFetch('/api/marketplace')
-  let devs = devUsers
+  // Setting all users who are developers into the marketplaceReducer
+  const { data: devUsers, fetchData: allDevs } = UseFetch("/api/marketplace");
+  let devs = devUsers;
   useEffect(() => {
-    props.setDevelopers(devs)
-  }, [devs])
+    props.setDevelopers(devs);
+  }, [devs]);
 
-    
-    // Rendering each developers info on marketplace
-    const developers = props.marketplaceReducer.allDevelopers
-    let mappedDevs
-    if(developers.length > 0){
-      mappedDevs = developers.map(dev => {
-        const encoded = encodeURIComponent(dev.email)
-        return (
-          <Link to={`/api/profile/${encoded}`}> 
-            <div key={dev.user_id} >
-              <img src={dev.profile_pic} />
-              <h1>{`${dev.first} ${dev.last}`}</h1>
-              <h2>{dev.title}</h2>
-              <h2>{dev.email}</h2>
-            </div>
-          </Link>
-        )
-      })
-    }
-   
+  // Rendering each developers info on marketplace
+  const developers = props.marketplaceReducer.allDevelopers;
+  let mappedDevs;
+  if (developers.length > 0) {
+    mappedDevs = developers.map(dev => {
+      const encoded = encodeURIComponent(dev.email);
+      return (
+        <Link to={`/api/profile/${encoded}`}>
+          <div key={dev.user_id}>
+            <img src={dev.profile_pic} />
+            <h1>{`${dev.first} ${dev.last}`}</h1>
+            <h2>{dev.title}</h2>
+            <h2>{dev.email}</h2>
+          </div>
+        </Link>
+      );
+    });
+  }
+
   return (
     <div>
       Mapped Devs
