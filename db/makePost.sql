2bad23038ad0e03f1a96d58694e3614a7bd9a79b
HSR <hectorsilvarobles@gmail.com>
2019-07-18 15:05:35 -0700

    profile posts

--- db/makePost.sql
+++ db/makePost.sql
@@ -2,6 +2,7 @@ insert into posts
     (content, time_entered, user_id)
 values($1, $2, $3);
 
-select *
+select users.first, users.last, posts.content, posts.time_entered, users.profile_pic, posts.post_id
 from posts
-where user_id = $3;
+inner join users on posts.user_id = users.user_id
+where posts.user_id = $3;
